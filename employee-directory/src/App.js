84caff4be572ac623c586325ccdebedbbeea266d
Lili Cohen <lili.cohen.tv@gmail.com>
2021-03-08 18:01:43 -0800

    working on search form by name of employee

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,13 +2,37 @@ import React, { Component } from 'react';
 import Table from "./components/Table";
 import Header from "./components/Header";
 import Rows from "./components/Rows";
+import SearchForm from "./components/SearchForm";
 import employee from "./employee.json";
 
   
 class App extends Component {
   
   state = {
-    employee
+    employee,
+    search: ""
+  };
+
+  searchPerson = (employee) => {
+    const results = employee.filter(employee =>
+      employee.toLowerCase().includes(employee.name.toLowerCase())
+    );
+    this.setState(results);// API.search(query)
+      // .then(res => this.setState({ result: res.data }))
+      // .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.setState(this.state.search);
   };
   
   render() {
@@ -17,14 +41,22 @@ class App extends Component {
           <Table>
             <Header />
           {this.state.employee.map(employee => (
-          <Rows
+            <Rows
+            key={employee.id}
             name={employee.name}
             occupation={employee.occupation}
             sign={employee.sign}
             food={employee.food}
             />
           ))}
-          </Table>
+        </Table>
+        <br />
+        <br />
+        <SearchForm
+        value={this.state.search}
+        handleInputChange={this.handleInputChange}
+        handleFormSubmit={this.handleFormSubmit}
+        />
       </div>
     );
   }
