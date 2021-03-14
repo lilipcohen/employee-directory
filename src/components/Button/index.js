7334d447e4cccdaa71f67427498e66e72dbe0217
Lili Cohen <lili.cohen.tv@gmail.com>
2021-03-14 14:19:02 -0700

    Updates

--- src/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Button(props) {
-    return (
-        <button onClick={() => props.aries(props.sign)} className="aries">Get All Aries!</button>
-    )
-}
-export default Button;
-
